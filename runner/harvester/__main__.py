b531ed1be1eda2087b4c7c4b52a8953bfa813c13
Miner (Bot) <miner@miner-node>
2018-07-18 15:33:57 +0300

    fill cache property by property, without threads, because server dies

--- runner/harvester/__main__.py
+++ runner/harvester/__main__.py
@@ -2,19 +2,10 @@ import os
 import threading
 from . import parse_repositories
 
-repositories = parse_repositories(os.path.join(os.path.dirname(__file__), '../../repositories.json'))
+repositories = parse_repositories(os.path.join(os.path.dirname(__file__), '../../repositories3.json'))
 
-threads = []
 for source_name, source in repositories.items():
     for reader_name, reader in source.items():
-        thread = threading.Thread(None, lambda reader: reader.create_cache(), '%s/%s' % (source_name, reader_name), (reader,))
-        print('Starting thread for %s' % thread.name)
-        thread.start()
-        threads.append(thread)
-
-while len(threads) > 0:
-    for thread in threads:
-        thread.join(1.0)
-        if not thread.is_alive():
-            print('Thread for %s finished' % thread.name)
-    threads = [thread for thread in threads if thread.is_alive()]
+        print('Starting reader %s' % reader_name)
+        reader.create_cache()
+        print('Reader %s finished' % reader_name)
