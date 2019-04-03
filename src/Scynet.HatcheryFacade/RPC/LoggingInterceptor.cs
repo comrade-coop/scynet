54ded89dc006999852bc7d35611da010cc405bca
Aleksandar Bonin <mrabonin@gmail.com>
2019-04-03 17:14:58 +0100

    Implemented an access check that also tracks the number of time it is called so we can track if we still have acces on every message send.

--- src/Scynet.HatcheryFacade/RPC/LoggingInterceptor.cs
+++ src/Scynet.HatcheryFacade/RPC/LoggingInterceptor.cs
@@ -14,6 +14,21 @@ namespace Scynet.HatcheryFacade.RPC
         private readonly ILogger<LoggingInterceptor> _logger;
         public LoggingInterceptor(ILogger<LoggingInterceptor> logger) => _logger = logger;
 
+        public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(TRequest request,
+            ClientInterceptorContext<TRequest, TResponse> context, AsyncServerStreamingCallContinuation<TRequest, TResponse> continuation)
+        {
+            try
+            {
+                return continuation(request, context);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError(err.ToString());
+                throw;
+            }
+        }
+
+
         public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
         {
             _logger.LogDebug($"Called from: {context.Host}, Method: {context.Method}, Peer: {context.Peer}, Status: {context.Deadline}");
