24ddd50b1ddd34a0b083c72f181688c536b77cbc
Viktor Velev <viktorvelev8@gmail.com>
2019-07-07 14:56:29 +0300

    Added default directories

---
+++ evaluator/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package ai.scynet.evaluator
+
+/**
+
+ * The first function to be executed
+ * @param args The arguments we are receiving
+ */
+fun main(args: Array<String>) {
+    println("Hello")
+}
+
---
+++ queen/src/main/kotlin/App.kt
@@ -0,0 +1,9 @@
+package ai.scynet.executor
+/**
+
+ * The first function to be executed
+ * @param args The arguments we are receiving
+ */
+fun main(args: Array<String>) {
+	println("Hello")
+}
