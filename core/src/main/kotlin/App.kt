5f402388a78b97860cd17ce1002983162673bd50
alex4o <mrabonin@gmail.com>
2019-07-07 10:33:28 +0000

    Added doc building and unit testing

--- core/src/main/kotlin/App.kt
+++ core/src/main/kotlin/App.kt
@@ -1,7 +1,26 @@
+package ai.scynet
+
 import org.apache.ignite.Ignition
 
+/**
+
+ * The first function to be executed
+ * @param args The arguments we are receiving
+ */
 fun main(args: Array<String>) {
     var ignite = Ignition.start()
 
     println(ignite)
 }
+
+
+/**
+ * @property data The number to show
+ */
+class Hello(val data: Int) {
+
+    init {
+        println(data)
+    }
+}
+
