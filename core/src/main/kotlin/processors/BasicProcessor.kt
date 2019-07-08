0e7b46f09457345350be69fb376a962bdbce20f3
Viktor Velev <viktorvelev8@gmail.com>
2019-07-08 18:11:45 +0300

    Basic wireframing

---
+++ core/src/main/kotlin/processors/BasicProcessor.kt
@@ -0,0 +1,38 @@
+package ai.scynet.core.processors
+
+import ai.scynet.core.descriptors.StreamDescriptor
+import org.apache.ignite.IgniteDataStreamer
+import org.apache.ignite.compute.ComputeTaskContinuousMapper
+
+class BasicProcessor: Processor {
+	/*
+		A very basic processor, which implements the core processor interface.
+		Optional constructor -> Initialize all data automatically
+	 */
+
+	constructor(descriptor: StreamDescriptor)
+
+	var inputStreams: IgniteDataStreamer<String, String>? = null
+	var outputStream: ComputeTaskContinuousMapper? = null
+	var streamId: String = ""
+	override var engageCount: Int
+		get() = TODO("not implemented")
+		set(value) {}
+
+	override var descriptor: StreamDescriptor
+		get() = TODO("not implemented")
+		set(value) {}
+
+	override fun process(): ComputeTaskContinuousMapper {
+		TODO("not implemented")
+	}
+
+	override var id: String
+		get() = TODO("not implemented")
+		set(value) {}
+
+	init {
+		this.engageCount = 0
+	}
+
+}
--- core/src/main/kotlin/processors/ProcessorFactory.kt
+++ core/src/main/kotlin/processors/ProcessorFactory.kt
@@ -1,4 +1,29 @@
 package ai.scynet.core.processors
 
+import ai.scynet.core.descriptors.StreamDescriptor
+import org.apache.ignite.Ignite
+import org.apache.ignite.IgniteCache
+
 class ProcessorFactory {
+
+	var state: IgniteCache<String,String>? = null
+
+	init {
+		//this.state!! = ignite.getOrCreateCache("PF:${uri.query.id}")
+		println("Initializing processor factory.")
+		println("stub")
+	}
+
+	fun create(streamDescriptor: StreamDescriptor): Processor {
+		// TODO: Finish discussing the StreamDescriptor and then implement parsing.
+		println("stub")
+
+		val created: Processor? = null // TODO: Create a simple processor that implements the interface
+		return created!!
+	}
+
+	fun list() {
+		// TODO: Return created processors and their status -> Possibly useful. Not high priority
+		println("stub")
+	}
 }
