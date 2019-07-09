8187ff0a780b322d2564dcfb6543dd3742f27904
Viktor Velev <viktorvelev8@gmail.com>
2019-07-09 17:58:50 +0300

    Defining processor descriptor

--- core/src/main/kotlin/descriptors/Processor.kt
+++ core/src/main/kotlin/descriptors/Processor.kt
@@ -1,16 +1,51 @@
 package ai.scynet.core.descriptors
 
 import ai.scynet.core.processors.Processor
+import java.io.ByteArrayInputStream
+import java.io.ObjectInputStream
+import java.util.Properties
 import java.net.URI
-import java.util.*
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
 
+
+// TODO: Move this to ai.scynet.common or leave in core after small refactoring
+fun bytesToHex(hashInBytes: ByteArray): String {
+
+	val sb = StringBuilder()
+	for (b in hashInBytes) {
+		sb.append(String.format("%02x", b))
+	}
+	return sb.toString()
+
+}
 
 data class ProcessorDescriptor(var uri: URI) {
-	var id: String = uri.query // UUID?
-	var source: String = uri.query
-	var proccessorClassName: String = uri.query // TODO: This should be an extended Processor -> Discussion
-	var problem: String = uri.query
-	var dependencies: ArrayList<StreamDescriptor>? = null
-	var output: StreamDescriptor? = null
+
+	var id: String = bytesToHex(
+			MessageDigest.getInstance("SHA-256").digest(
+					uri.toString().toByteArray(StandardCharsets.UTF_8)
+		) // idk hash of the URI - using as ID TODO: Discuss, since this is somewhat stupid
+	)
+	var ownerId: String = uri.authority
+	var problem: String = uri.fragment
+
+	/*
+		Java Properties
+		Contains a Processor extended class, and all kinds of different information
+		in order to create a working processor
+	*/
+	var proccessorProperties: Properties = ObjectInputStream(
+			uri.query.byteInputStream(StandardCharsets.UTF_8) // TODO: WARNING! Get the specific parameter, not working right now
+	).readObject() as Properties
+
+	init {
+		// TODO: Define properties file, implement the property assigning here
+		var dependencies: ArrayList<StreamDescriptor>? = null
+		var output: StreamDescriptor? = null
+		var processorClass: Processor? = null
+
+	}
+
 
 }
