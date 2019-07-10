778d5293805ef0002318b0f9e38f3f04e17ce448
alex4o <mrabonin@gmail.com>
2019-07-10 14:20:20 +0300

    Id is now UUID with the uri namespace

--- core/src/main/kotlin/descriptors/Processor.kt
+++ core/src/main/kotlin/descriptors/Processor.kt
@@ -3,10 +3,11 @@ package ai.scynet.core.descriptors
 import ai.scynet.core.processors.Processor
 import java.io.ByteArrayInputStream
 import java.io.ObjectInputStream
-import java.util.Properties
 import java.net.URI
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 // TODO: Move this to ai.scynet.common or leave in core after small refactoring
@@ -20,13 +21,15 @@ fun bytesToHex(hashInBytes: ByteArray): String {
 
 }
 
+fun fromUri(uri: URI): Properties {
+	val properties = Properties()
+	properties.load(uri.query.replace('&', '\n').byteInputStream())
+	return properties
+}
+
 data class ProcessorDescriptor(var uri: URI) {
 
-	var id: String = bytesToHex(
-			MessageDigest.getInstance("SHA-256").digest(
-					uri.toString().toByteArray(StandardCharsets.UTF_8)
-		) // idk hash of the URI - using as ID TODO: Discuss, since this is somewhat stupid
-	)
+	var id: UUID = UUID.fromString(uri.toString())
 	var ownerId: String = uri.authority
 	var problem: String = uri.fragment
 
@@ -35,9 +38,7 @@ data class ProcessorDescriptor(var uri: URI) {
 		Contains a Processor extended class, and all kinds of different information
 		in order to create a working processor
 	*/
-	var proccessorProperties: Properties = ObjectInputStream(
-			uri.query.byteInputStream(StandardCharsets.UTF_8) // TODO: WARNING! Get the specific parameter, not working right now
-	).readObject() as Properties
+	var properties: Properties = fromUri(uri)
 
 	init {
 		// TODO: Define properties file, implement the property assigning here
@@ -46,6 +47,8 @@ data class ProcessorDescriptor(var uri: URI) {
 		var processorClass: Processor? = null
 
 	}
+
+
 
 
 }
