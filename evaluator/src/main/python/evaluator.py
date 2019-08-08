e4d93ff0c665995a7eb7b7360ccc279ea2e5d5d6
Viktor Velev <viktorvelev8@gmail.com>
2019-08-09 02:25:40 +0300

    Merge and sync with core_processing

--- evaluator/src/main/python/evaluator.py
+++ evaluator/src/main/python/evaluator.py
@@ -5,7 +5,7 @@ class Evaluator(abc.ABC):
     '''
         Evaluator interface that needs to be implemented by any custom evaluator
         ABC - Abstract Base Class
-    '''    
+    '''
     @abc.abstractmethod
     def loss(self, y_true, y_pred):
         '''
