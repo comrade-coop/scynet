af184131c289d9874781c6b5b7a1ea06df94e31e
Viktor Velev <viktorvelev8@gmail.com>
2019-08-13 14:56:15 +0300

    Added a basic reinforcement evaluator interface

--- evaluator/src/main/python/evaluator.py
+++ evaluator/src/main/python/evaluator.py
@@ -1,10 +1,35 @@
 import abc
 
+class ReinforcementEnvironment(abc.ABC):
+    '''
+        Based on the OpenAI GYM standard
+    '''
+
+    @abc.abstractmethod
+    def reset():
+        pass
+
+    @abc.abstractmethod
+    def get_observations():
+        pass
+
+    @abc.abstractmethod
+    def get_initial_observations():
+        pass
+    '''
+        Interface
+         - observables = {}
+         - get_observations: Observations {}
+         - Other methods for modifying state and working with the environment
+        Should implement different ways to engage and modify state and check observation data
+    '''
+    pass
 
 class Evaluator(abc.ABC):
     '''
         Evaluator interface that needs to be implemented by any custom evaluator
         ABC - Abstract Base Class
+        TODO Discuss
     '''
     @abc.abstractmethod
     def loss(self, y_true, y_pred):
@@ -12,3 +37,26 @@ class Evaluator(abc.ABC):
             Should return a tensorflow differentiable function
         '''
         pass
+
+
+class REvaluator(abc.ABC):
+    '''
+        Evaluator interface that needs to be implemented by any reinforcement evaluator
+        ABC - Abstract Base Class
+        TODO Discuss
+    '''
+    @abc.abstractmethod
+    def environment():
+        '''
+            Should return an already implemented ReinforcementEnvironment class and set self.environment
+        '''
+        pass
+    
+
+    @abc.abstractmethod
+    def reward(self):
+        '''
+            Should return a reward based on the environment state
+        '''
+        pass
+    
