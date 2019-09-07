ecf75a1ed7b7c96312132ddb4a9c3e990f2ad286
Viktor Velev <viktorvelev8@gmail.com>
2019-09-08 00:35:10 +0300

    Trainin Jobs Pipeline, MockService, TrainingJobStream, MockJobStream, Argparsing and process comunication

---
+++ trainer/src/main/kotlin/mock/MockJobsStream.kt
@@ -0,0 +1,17 @@
+package ai.scynet.trainer
+
+import ai.scynet.protocol.TrainingJob
+import descriptors.Properties
+import processors.ILazyStreamService
+import processors.LazyStream
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.reflect.KClass
+
+class MockJobsStream : LazyStream<TrainingJob>{
+    override val streamServiceClass: KClass<out ILazyStreamService> = MockService::class
+    override val classId: String = "mockJobsStream"
+
+    constructor(): super()
+    constructor(id: UUID, inputStreamIds: ArrayList<UUID>?, properties: Properties): super(id, inputStreamIds, properties)
+}
